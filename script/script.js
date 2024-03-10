a17d2ec036612ed5952c91a971de7d601e651504
NikolaPejchinovski <niklaus420nikola@gmail.com>
2024-03-10 12:32:03 +0100

    contact

--- script/script.js
+++ script/script.js
@@ -3,9 +3,67 @@ document.addEventListener("DOMContentLoaded", function () {
   const copyrightYear = document.querySelector(".copyright-year");
   const startElevating = document.querySelectorAll(".maximize-potential");
   const contactContainer = document.querySelector(".contact-container");
+  const contactForm = document.querySelector(".contact-form");
+  const contactFormContainer = document.querySelectorAll(
+    ".contact-form-container *"
+  );
+  const successMessage = document.getElementById("successMessage");
   const closeBtn = document.querySelector(".close");
 
   document.documentElement.style.overflowX = "hidden";
+  // c136063d-0709-4cd2-8c8d-c46a329b956f
+
+  // Contact Form Submit
+  contactForm.addEventListener("submit", (e) => {
+    submitForm(e);
+  });
+
+  // Function to submit the contact form
+  function submitForm(e) {
+    e.preventDefault();
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let message = document.getElementById("message");
+    const subject = `Message from ${name.value}`;
+    const body = `Name: ${name.value} <br/> Email: ${email.value} <br/> Subject: ${subject} <br/> Message: ${message.value}`;
+
+    Email.send({
+      SecureToken: "270a7911-8260-4b96-a86f-113571ec50fc",
+      To: "mangodudenikola@gmail.com",
+      From: email.value,
+      Subject: subject,
+      Body: body,
+    }).then(() => {
+      displaySuccessMessage();
+      clearFormFields();
+    });
+  }
+
+  // Function to display the success message
+  function displaySuccessMessage() {
+    contactFormContainer.forEach((element) =>
+      element.classList.add("invisible")
+    );
+
+    successMessage.style.display = "flex";
+
+    setTimeout(() => {
+      contactFormContainer.forEach((element) =>
+        element.classList.remove("invisible")
+      );
+      successMessage.style.display = "none";
+    }, 5000);
+  }
+
+  // Function to clear the form fields
+  function clearFormFields() {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let message = document.getElementById("message");
+    name.value = "";
+    email.value = "";
+    message.value = "";
+  }
 
   // Open Contact Form
 
