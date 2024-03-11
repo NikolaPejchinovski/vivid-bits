055123e00044c117dfa90304c616635288ea02f4
NikolaPejchinovski <niklaus420nikola@gmail.com>
2024-03-11 14:26:34 +0100

    contact form changes

--- script/script.js
+++ script/script.js
@@ -5,25 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const startElevating = document.querySelectorAll(".maximize-potential");
   const contactContainer = document.querySelector(".contact-container");
   const contactForm = document.querySelector(".contact-form");
-  const contactFormContainer = document.querySelectorAll(
-    ".contact-form-container *"
-  );
   const successMessage = document.getElementById("successMessage");
   const closeBtn = document.querySelector(".close");
 
+  // Disable horizontal scrolling
   document.documentElement.style.overflowX = "hidden";
 
   // Contact Form Submit
-  contactForm.addEventListener("submit", (e) => {
-    submitForm(e);
-  });
+  contactForm.addEventListener("submit", submitForm);
 
   // Function to submit the contact form
   function submitForm(e) {
     e.preventDefault();
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
-    let message = document.getElementById("message");
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
     const subject = `Message from ${name.value}`;
     const body = `Name: ${name.value} <br/> Email: ${email.value} <br/> Subject: ${subject} <br/> Message: ${message.value}`;
 
@@ -41,53 +37,50 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Function to display the success message
   function displaySuccessMessage() {
-    contactFormContainer.forEach((element) =>
-      element.classList.add("invisible")
+    const contactFormContainer = document.querySelector(
+      ".contact-form-container"
     );
 
+    contactFormContainer.classList.add("invisible");
     successMessage.style.display = "flex";
 
     setTimeout(() => {
-      contactFormContainer.forEach((element) =>
-        element.classList.remove("invisible")
-      );
+      contactFormContainer.classList.remove("invisible");
       successMessage.style.display = "none";
-    }, 5000);
+    }, 3000);
   }
 
   // Function to clear the form fields
   function clearFormFields() {
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
-    let message = document.getElementById("message");
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const message = document.getElementById("message");
     name.value = "";
     email.value = "";
     message.value = "";
   }
 
   // Open Contact Form
-
   getStarted.addEventListener("click", () =>
     contactContainer.classList.add("open")
   );
 
   startElevating.forEach((button) => {
-    button.addEventListener("click", () => {
-      contactContainer.classList.add("open");
-    });
+    button.addEventListener("click", () =>
+      contactContainer.classList.add("open")
+    );
   });
 
   // Close Contact Form
-
-  closeBtn.addEventListener("click", () => {
-    contactContainer.classList.remove("open");
-  });
+  closeBtn.addEventListener("click", () =>
+    contactContainer.classList.remove("open")
+  );
 
   // Get and automatically update the copyright year
   loadCopyrightYear();
 
   // Testimonials Swipe Effect
-  const swiper = new Swiper(".swiper", {
+  new Swiper(".swiper", {
     grabCursor: true,
     spaceBetween: 30,
     pagination: {
@@ -102,37 +95,33 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Load Particles
-  particlesJS.load("particles-js", "script/particles.json", function () {
-    console.log("callback - particles.js config loaded");
-  });
-  particlesJS.load("particles-js2", "script/particles.json", function () {
-    console.log("callback - particles.js config loaded");
+  ["particles-js", "particles-js2"].forEach((elementId) => {
+    particlesJS.load(elementId, "script/particles.json", () => {
+      console.log("callback - particles.js config loaded");
+    });
   });
 
   // Smooth scrolling for anchor links
   document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      target.scrollIntoView({ behavior: "smooth" });
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
     });
   });
 
   // Event listener for header opacity change on scroll
-  const eventListener = "scroll";
-  document.addEventListener(eventListener, handleHeaderOpacity);
+  document.addEventListener("scroll", handleHeaderOpacity);
 
   function handleHeaderOpacity() {
     const y = window.scrollY || window.pageYOffset;
-    if (y === 0) headerBackground.style.opacity = 0;
-    const opacity = map(y, 400, 800, 0, 1);
-    headerBackground.style.opacity = y > 400 ? opacity : 0;
+    headerBackground.style.opacity = y === 0 ? 0 : map(y, 400, 800, 0, 1);
   }
 
   // Get and automatically update the copyright year
   function loadCopyrightYear() {
-    const currentYear = new Date().getFullYear(); // Get current year
-    copyrightYear.innerHTML = currentYear; // Update the year on screen(footer)
+    copyrightYear.innerHTML = new Date().getFullYear();
   }
 });
 
